bc7f6a67bd95b9a0c9534ca7ec7e9461a2f68bdc
sondor-dev <sondortechnology@gmail.com>
2025-06-18 22:20:20 +0100

    fix: change default error messages to public

--- Sondor.Errors/Sondor.Errors/SondorErrorMessages.cs
+++ Sondor.Errors/Sondor.Errors/SondorErrorMessages.cs
@@ -3,65 +3,65 @@
 /// <summary>
 /// Collection of error messages.
 /// </summary>
-internal static class SondorErrorMessages
+public static class SondorErrorMessages
 {
     /// <summary>
     /// The bad request error message.
     /// </summary>
-    internal const string BadRequest =
+    public const string BadRequest =
         "Unfortunately, '{0}' validations failed.";
 
     /// <summary>
     /// The forbidden error message.
     /// </summary>
-    internal const string Forbidden =
+    public const string Forbidden =
         "Unfortunately, you do not have permission to access the request resource";
 
     /// <summary>
     /// The resource already exists.
     /// </summary>
-    internal const string ResourceAlreadyExists =
+    public const string ResourceAlreadyExists =
         "Unfortunately, an '{0}' with an '{1}' of '{2}' already exists.";
 
     /// <summary>
     /// The resource create failed.
     /// </summary>
-    internal const string ResourceCreateFailed =
+    public const string ResourceCreateFailed =
         "Unfortunately, the create '{0}' request failed.";
 
     /// <summary>
     /// The resource delete failed.
     /// </summary>
-    internal const string ResourceDeleteFailed =
+    public const string ResourceDeleteFailed =
         "Unfortunately, the delete '{0}' request failed.";
 
     /// <summary>
     /// The resource not found error messages.
     /// </summary>
-    internal const string ResourceNotFound =
+    public const string ResourceNotFound =
         "Unfortunately, an '{0}' with an '{1}' of '{2}' was not found.";
 
     /// <summary>
     /// The resource patch failed.
     /// </summary>
-    internal const string ResourcePatchFailed =
+    public const string ResourcePatchFailed =
         "Unfortunately, the patch request failed.";
 
     /// <summary>
     /// The resource update failed.
     /// </summary>
-    internal const string ResourceUpdateFailed =
+    public const string ResourceUpdateFailed =
         "Unfortunately, the update '{0}' request failed.";
 
     /// <summary>
     /// The unauthorized error message.
     /// </summary>
-    internal const string Unauthorized =
+    public const string Unauthorized =
         "Unfortunately, you're not authorized to access the requested resource.";
 
     /// <summary>
     /// The unexpected error.
     /// </summary>
-    internal const string UnexpectedError =
+    public const string UnexpectedError =
         "{0}";
 }
