6ef278485ceffd51f1a893598768d8ea3b91f515
sondor-dev <sondortechnology@gmail.com>
2025-07-24 18:40:03 +0100

    feat: add more exceptions

---
+++ Sondor.Errors/Sondor.Errors/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace Sondor.Errors.Exceptions;
+
+/// <summary>
+/// Forbidden exception is thrown for forbidden requests.
+/// </summary>
+/// <remarks>
+/// Create a new instance of the <see cref="ForbiddenException"/> class with a specified error message.
+/// </remarks>
+public class ForbiddenException(string message) :
+    Exception(message);
---
+++ Sondor.Errors/Sondor.Errors/Exceptions/UnauthorisedException.cs
@@ -0,0 +1,10 @@
+﻿namespace Sondor.Errors.Exceptions;
+
+/// <summary>
+/// Unauthorised exception is thrown for unauthorised requests.
+/// </summary>
+/// <remarks>
+/// Creates a new instance of the <see cref="UnauthorisedException"/> class with a specified error message.
+/// </remarks>
+public class UnauthorisedException(string message) :
+    Exception(message);
