127fdff6cd7a0f13da4cc9bea580d64545cc186d
sondor-dev <sondortechnology@gmail.com>
2025-07-24 18:46:54 +0100

    feat: add exception tests

---
+++ Sondor.Errors/Sondor.Errors.Tests/Exceptions/ForbiddenExceptionTests.cs
@@ -0,0 +1,29 @@
+﻿using Sondor.Errors.Exceptions;
+
+namespace Sondor.Errors.Tests.Exceptions;
+
+/// <summary>
+/// Tests for <see cref="ForbiddenException"/>.
+/// </summary>
+public class ForbiddenExceptionTests
+{
+    /// <summary>
+    /// Ensures that <see cref="ForbiddenException"/> constructor works as expected.
+    /// </summary>
+    [Test]
+    public void Constructor()
+    {
+        // arrange
+        const string message = "Access to this resource is forbidden.";
+        
+        // act
+        var exception = new ForbiddenException(message);
+        
+        // assert
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(exception.Message, Is.EqualTo(message));
+            Assert.That(exception, Is.InstanceOf<ForbiddenException>());
+        }
+    }
+}
---
+++ Sondor.Errors/Sondor.Errors.Tests/Exceptions/RequestInvalidExceptionTests.cs
@@ -0,0 +1,30 @@
+﻿using Sondor.Errors.Exceptions;
+
+namespace Sondor.Errors.Tests.Exceptions;
+
+/// <summary>
+/// Tests for <see cref="RequestInvalidException"/>.
+/// </summary>
+[TestFixture]
+public class RequestInvalidExceptionTests
+{
+    /// <summary>
+    /// Ensures that <see cref="RequestInvalidException"/> constructor works as expected.
+    /// </summary>
+    [Test]
+    public void Constructor()
+    {
+        // arrange
+        const string message = "The request is invalid.";
+        
+        // act
+        var exception = new RequestInvalidException(message);
+
+        // assert
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(exception.Message, Is.EqualTo(message));
+            Assert.That(exception, Is.InstanceOf<RequestInvalidException>());
+        }
+    }
+}
